716a0cf898f881e811b9342542595a279bb83dfe
Benjamin Roberts <Broberts23505@columbushs.com>
2021-08-24 19:53:51 -0400

    commit saved

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const cityForm = document.querySelector('form');
 const card = document.querySelector('.card');
 const details = document.querySelector('.details');
+const time = document.querySelector('img.time');
+const icon = document.querySelector('.icon img');
 
 const updateUI = (data) => {
-
   // destructure properties
   const { cityDets, weather } = data;
 
@@ -16,16 +17,33 @@ const updateUI = (data) => {
       <span>&deg;C</span>
     </div>
   `;
+
+  // update the night/day & icon images
+  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  icon.setAttribute('src', iconSrc);
+  
+  let timeSrc = null;
+  if(weather.IsDayTime){
+    timeSrc = 'img/day.svg';
+  } else {
+    timeSrc = 'img/night.svg';
+  }
+  time.setAttribute('src', timeSrc);
+
   // remove the d-none class if present
   if(card.classList.contains('d-none')){
     card.classList.remove('d-none');
   }
 };
+
 const updateCity = async (city) => {
+
   const cityDets = await getCity(city);
   const weather = await getWeather(cityDets.Key);
   return { cityDets, weather };
+
 };
+
 cityForm.addEventListener('submit', e => {
   // prevent default action
   e.preventDefault();
@@ -33,6 +51,7 @@ cityForm.addEventListener('submit', e => {
   // get city value
   const city = cityForm.city.value.trim();
   cityForm.reset();
+
   // update the ui with new city
   updateCity(city)
     .then(data => updateUI(data))
